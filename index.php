7a503873faaed1f62195baa40832b123ae02fe1b
aldusosi <pocmusic321@gmail.com>
2024-05-28 13:08:31 -0300

    Criando index.php pag form e estilizando

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title></title>
+		<link rel="stylesheet" type="text/css" href="./css/index.css">
+	</head>
+	<body>
+		<div class="header">
+			<h1>App Help Desk</h1>
+		</div>
+
+		<div class="body">
+			<div class="container-form">
+
+				<div class="head-form">
+					<h3>Login</h3>
+				</div>
+
+				<div class="body-form">
+					<form mothod="post">
+						<div class="email-form">
+							<input type="email" name="email" placeholder="Email">
+						</div>
+						<div class="senha-form">
+							<input type="password" name="senha" placeholder="Senha">
+						</div>
+						<div>
+							<button type="submit">Entrar</button>
+						</div>
+					</form>
+				</div>
+
+			</div>
+		</div>
+	</body>
+</html>
