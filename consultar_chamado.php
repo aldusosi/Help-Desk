0ba53bdb9cdf4222d7f31e61192858781ff658fb
aldusosi <desenho2510@gmail.com>
2024-06-01 00:48:26 -0300

    Implementando consultar chamado

--- consultar_chamado.php
+++ consultar_chamado.php
@@ -1,4 +1,36 @@
 <?php require_once 'validador_acesso.php' ?>
-CONSULTAR CHAMADO
-<rh>
-<div><a href="logoff.php">Sair</a></div>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Consultar Chamado</title>
+		<link rel="stylesheet" type="text/css" href="./css/consultar_chamado.css">
+	</head>
+
+	<body>
+		<div class="header">
+			<h1>App Help Dask</h1>
+		</div>
+		<div class="body-chamado">
+			<div class="container-consultar">
+				<div class="container-head">
+					<h3>Consultar Chamado</h3>
+				</div>
+				<div class="container-body">
+					<div class="chamado">
+						<h2>Título</h2>
+						<h3>Categoria</h3>
+						<p>Descricao</p>
+					</div>
+					<div class="chamado">
+						<h2>Título</h2>
+						<h3>Categoria</h3>
+						<p>Descricao</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
